f9cdb07746f3b8dfc37609e7049db107aa7f71ab
raquelgismero <raquelgismero2@gmail.com>
2026-07-15 14:09:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ with tab_aprender:
                 
                 try:
                     response = client.models.generate_content(
-                        model='gemini-2.0-flash',
+                        model='gemini-2.5-flash-lite'
                         contents=prompt,
                         config=types.GenerateContentConfig(
                             response_mime_type="application/json",
@@ -165,7 +165,7 @@ with tab_aprender:
                     
                     )
                     st.session_state.chat_ia = client.chats.create(
-                        model="gemini-2.0-flash",
+                        model="gemini-2.5-flash-lite",
                         config=types.GenerateContentConfig(system_instruction=instrucciones_chat)
                     )
                     st.session_state.mensajes = []
